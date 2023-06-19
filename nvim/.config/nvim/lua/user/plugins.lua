03047927c1094024690284abb72ce231d1537849
ibrahim <ibrahim@mixrank.com>
2023-06-19 17:04:40 +0300

    remove bracket guides from nvim plugins

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,7 +1,6 @@
 return {
   -- You can also add new plugins here as well:
   {"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
-  {'fedepujol/bracket-guides', lazy = false},
   {"folke/tokyonight.nvim", lazy = false},
   {"lunarvim/Onedarker.nvim", lazy = false},
   {
